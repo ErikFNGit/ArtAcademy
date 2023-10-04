4cf7a57fac961f793df89ffcf9ac712661f9e86c
Joel <joel.gurrera@inslapineda.cat>
2023-10-04 10:27:54 +0200

    Cambios

--- listarCursos.php
+++ listarCursos.php
@@ -26,25 +26,39 @@
             include("Funciones.php");
             if(isset($_SESSION["userType"])){
         ?>
-    </div>
-    <form action="listarCursos.php" method="POST">
-        <label for="codigo">Buscador:</label>
-        <input type="text" name="busqueda">
-        <input type="submit" value="Aceptar">
-    </form>
-    <h1>CURSOS</h1>
-
-    </br>
-        <?php
-            if(isset($_POST["busqueda"])){
-                listaCursos(conexion(),$_POST["busqueda"],$_SESSION["userType"]);
+        <form action="listarCursos.php" method="POST">
+            <label for="codigo">Buscador:</label>
+            <input type="text" name="busqueda">
+            <input type="submit" value="Aceptar">
+        </form>
+        <div class="tabla">
+            <h1>CURSOS</h1>
+            <?php
+                if(isset($_POST["busqueda"])){
+                    listaCursos(conexion(),$_POST["busqueda"],$_SESSION["userType"]);
+                }else{
+                    listaCursos(conexion(),"",$_SESSION["userType"]);
+                }
             }else{
-                listaCursos(conexion(),"",$_SESSION["userType"]);
+                echo "<h1>No tienes acceso a esta pagina</h1>";
+                echo "<meta http-equiv='refresh' content ='2; url=index.php'>";
             }
-        }else{
-            echo "<h1>No tienes acceso a esta pagina</h1>";
-            echo "<meta http-equiv='refresh' content ='2; url=index.php'>";
-        }
-    ?>
+            ?>
+        </div>
+    </div>
+    <footer>
+        <img src="logoBlanco.png" alt="Logo de la academia con la letra en blanco" width="200px" height="100px">
+        <div>
+            Calle Invent, 69 08917 Badalona <br>
+            +34 677 424 950 <br>
+        </div>
+        <div>
+            <ul>   
+                <li><a href="">Instagram</a></li>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Twitter</a></li>
+            </ul>  
+        </div>
+    </footer>
 </body>
 </html>
