8dd9e9e235bccf2542d4d3d6f1bdd8d20f2fc7fa
Joel <joel.gurrera@inslapineda.cat>
2023-10-25 10:31:16 +0200

    cursosPublicos estilizada

--- cursosPublicos.php
+++ cursosPublicos.php
@@ -11,6 +11,59 @@
     <link rel="stylesheet" href="mainSCSS.css">
 </head>
 <body>
-    <?php cursosDisponibles(conexion()); ?>
+    <?php
+        if (!isset($_SESSION['userType'])){
+    ?>
+    <header>
+        <div>
+            <img src="logoNegro.png" alt="Logo de la academia con la letra en negro" width="100px" height="50px">
+        </div>
+        <div>
+            <a href="paginaPrincipal.php">Inicio</a>
+        </div>
+        <div>
+            <a href="index.php"> Iniciar Sesion </a>
+        </div>
+        <div>
+            <a href="registrarAlumno.php"> Registrarse </a>
+        </div>
+    </header>
+    <?php
+    }else{
+    ?>
+    <header>
+        <div>
+            <img src="logoNegro.png" alt="Logo de la academia con la letra en negro" width="100px" height="50px">
+        </div>
+        <div>
+            <a href="paginaPrincipal.php">Inicio</a>
+        </div>
+        <div>
+            <a href="perfilAlumno.php"> Mi perfil </a>
+        </div>
+        <div>
+            <a href="cerrarSesion.php"> Cerrar Sesion</a>
+        </div>
+    </header>
+    <?php } ?>
+    
+    <div class="disponibles">
+        <?php cursosDisponibles(conexion()); ?>
+    </div>
+
+    <footer>
+        <img src="logoBlanco.png" alt="Logo de la academia con la letra en blanco" width="100px" height="50px">
+        <div>
+            Calle Invent, 69 08917 Badalona <br>
+            +34 677 424 950 <br>
+        </div>
+        <div>
+            <ul>   
+                <li><a href="">Instagram</a></li>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Twitter</a></li>
+            </ul>  
+        </div>
+    </footer>
 </body>
 </html>
